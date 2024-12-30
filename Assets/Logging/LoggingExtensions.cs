95c841e32f9708f72cf07103b818abb98d877a64
cz <cyz36@pm.me>
2024-12-30 14:34:43 +0800

    more on logger

--- Assets/Logging/LoggingExtensions.cs
+++ Assets/Logging/LoggingExtensions.cs
@@ -10,20 +10,47 @@ namespace dss.pub.logging {
 
 		private static bool initialized = false;
 
-		public static void log(this object obj, string message) {
+		private static void CheckLogger() {
 			if (!initialized) {
-				Log.Logger = new(new LoggerConfig().MinimumLevel.Info().WriteTo.File(filePath));
+				Log.Logger = new(new LoggerConfig()
+					.MinimumLevel.Info()
+					.OutputTemplate("[{Level} {Timestamp}] {Message}{NewLine}{Stacktrace}")
+					.WriteTo.File(filePath)
+					.WriteTo.StdOut(minLevel: LogLevel.Warning));
 				initialized = true;
 			}
+		}
 
-			Log.Info(message);
+		private static string MakeMessage(object key, string message) {
+			return message + "[" + key switch {
+				string s => s,
+				Component component => component.gameObject.name + "." + component.name,
+				Object unityObj => unityObj.name,
+				_ => key.ToString()
+			} + "]";
+		}
+
+
+		public static void log(this object key, string message) {
+			CheckLogger();
+			Log.Info(MakeMessage(key, message));
+		}
+
+		public static void warn(this object key, string message) {
+			CheckLogger();
+			Log.Warning(MakeMessage(key, message));
+		}
+
+		public static void err(this object key, string message) {
+			CheckLogger();
+			Log.Error(MakeMessage(key, message));
 		}
 
 #if (UNITY_EDITOR)
-		public static void print(this object obj, params object[] args) {
+		public static void print(this object key, params object[] args) {
 			var sb = new StringBuilder();
 			sb.AppendJoin(", ", args);
-			if (obj is Object unityObj) {
+			if (key is Object unityObj) {
 				Debug.Log(sb.ToString(), unityObj);
 			} else {
 				Debug.Log(sb.ToString());
