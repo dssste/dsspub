ccc0428554f1bdd6656b6b9bf2f7802a5ba38a2b
cz <cyz36@pm.me>
2024-12-30 11:26:32 +0800

    add a namespace

--- Assets/Logging/LoggingExtensions.cs
+++ Assets/Logging/LoggingExtensions.cs
@@ -2,31 +2,32 @@ using System.Text;
 using Unity.Logging;
 using UnityEngine;
 
-// not putting this in a namespace so you don't have to type `using` every time
-public static class DSSLoggingExtensions {
-	public static void log(this object obj, string message) {
-		Log.Info(message);
-	}
+namespace dss.pub.logging {
+	public static class LoggingExtensions {
+		public static void log(this object obj, string message) {
+			Log.Info(message);
+		}
 
 #if (UNITY_EDITOR)
-	public static void print(this object obj, params object[] args) {
-		var sb = new StringBuilder();
-		sb.AppendJoin(", ", args);
-		if (obj is Object unityObj) {
-			Debug.Log(sb.ToString(), unityObj);
-		} else {
-			Debug.Log(sb.ToString());
+		public static void print(this object obj, params object[] args) {
+			var sb = new StringBuilder();
+			sb.AppendJoin(", ", args);
+			if (obj is Object unityObj) {
+				Debug.Log(sb.ToString(), unityObj);
+			} else {
+				Debug.Log(sb.ToString());
+			}
 		}
-	}
 
-	public static void iprint(this Object obj, params object[] args) {
-		var go = obj switch {
-			Component c => c.gameObject,
-			_ => obj,
-		};
-		if (UnityEditor.Selection.activeObject == go) {
-			print(go, args);
+		public static void iprint(this Object obj, params object[] args) {
+			var go = obj switch {
+				Component c => c.gameObject,
+				_ => obj,
+			};
+			if (UnityEditor.Selection.activeObject == go) {
+				print(go, args);
+			}
 		}
-	}
 #endif
+	}
 }
